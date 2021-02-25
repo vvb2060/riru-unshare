485766927361341de20434ceb6ca78974d8778c7
vvb2060 <vvb2060@gmail.com>
2021-02-26 03:52:20 +0800

    1.0

---
+++ module/src/main/cpp/main.cpp
@@ -0,0 +1,101 @@
+#include <jni.h>
+#include <sys/types.h>
+#include <riru.h>
+#include <malloc.h>
+#include <cstring>
+
+#include "logging.h"
+#include "nativehelper/scoped_utf_chars.h"
+#include "android_filesystem_config.h"
+
+static void forkAndSpecializePre(
+        JNIEnv *env, jclass clazz, jint *uid, jint *gid, jintArray *gids, jint *runtimeFlags,
+        jobjectArray *rlimits, jint *mountExternal, jstring *seInfo, jstring *niceName,
+        jintArray *fdsToClose, jintArray *fdsToIgnore, jboolean *is_child_zygote,
+        jstring *instructionSet, jstring *appDataDir, jboolean *isTopApp,
+        jobjectArray *pkgDataInfoList,
+        jobjectArray *whitelistedDataInfoList, jboolean *bindMountAppDataDirs,
+        jboolean *bindMountAppStorageDirs) {
+    if (*mountExternal == 0) {
+        *mountExternal = 1;
+        ScopedUtfChars name(env, *niceName);
+        LOGI("unshare uid=%d name=%s", *uid, name.c_str());
+    }
+}
+
+
+static void specializeAppProcessPre(
+        JNIEnv *env, jclass clazz, jint *uid, jint *gid, jintArray *gids, jint *runtimeFlags,
+        jobjectArray *rlimits, jint *mountExternal, jstring *seInfo, jstring *niceName,
+        jboolean *startChildZygote, jstring *instructionSet, jstring *appDataDir,
+        jboolean *isTopApp, jobjectArray *pkgDataInfoList, jobjectArray *whitelistedDataInfoList,
+        jboolean *bindMountAppDataDirs, jboolean *bindMountAppStorageDirs) {
+    if (*mountExternal == 0) {
+        *mountExternal = 1;
+        ScopedUtfChars name(env, *niceName);
+        LOGI("unshare uid=%d name=%s", *uid, name.c_str());
+    }
+}
+
+
+static int shouldSkipUid(int uid) {
+    int appid = uid % AID_USER_OFFSET;
+    if (appid >= AID_APP_START && appid <= AID_APP_END) return false;
+    if (appid >= AID_ISOLATED_START && appid <= AID_ISOLATED_END) return false;
+    return false;
+}
+
+
+extern "C" {
+
+int riru_api_version;
+RiruApiV9 *riru_api_v9;
+
+void *init(void *arg) {
+    static int step = 0;
+    step += 1;
+
+    static void *_module;
+
+    switch (step) {
+        case 1: {
+            auto core_max_api_version = *(int *) arg;
+            riru_api_version = core_max_api_version <= RIRU_MODULE_API_VERSION ?
+                               core_max_api_version : RIRU_MODULE_API_VERSION;
+            return &riru_api_version;
+        }
+        case 2: {
+            switch (riru_api_version) {
+                // RiruApiV10 and RiruModuleInfoV10 are equal to V9
+                case 10:
+                case 9: {
+                    riru_api_v9 = (RiruApiV9 *) arg;
+
+                    auto module = (RiruModuleInfoV9 *) malloc(sizeof(RiruModuleInfoV9));
+                    memset(module, 0, sizeof(RiruModuleInfoV9));
+                    _module = module;
+
+                    module->supportHide = true;
+
+                    module->version = RIRU_MODULE_VERSION;
+                    module->versionName = RIRU_MODULE_VERSION_NAME;
+                    module->shouldSkipUid = shouldSkipUid;
+                    module->forkAndSpecializePre = forkAndSpecializePre;
+                    module->specializeAppProcessPre = specializeAppProcessPre;
+                    return module;
+                }
+                default: {
+                    return nullptr;
+                }
+            }
+        }
+        case 3: {
+            free(_module);
+            return nullptr;
+        }
+        default: {
+            return nullptr;
+        }
+    }
+}
+}
